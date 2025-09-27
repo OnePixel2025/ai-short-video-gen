0964fe1b39121c2748b2b75f41f5cb01b5e253bd
OnePixel2025 <onepixleagency@email.com>
2025-09-27 22:26:37 +0300

    Update auth domain

--- configs/firebaseConfig.jsx
+++ configs/firebaseConfig.jsx
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ai-short-video-generator-eefec.firebaseapp.com",
+  authDomain: "app.snapshort.io",
   projectId: "ai-short-video-generator-eefec",
   storageBucket: "ai-short-video-generator-eefec.firebasestorage.app",
   messagingSenderId: "491074912236",
