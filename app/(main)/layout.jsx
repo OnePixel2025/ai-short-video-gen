f30d4a4696e0d95410c1fd15cf04d564686e3565
OnePixel2025 <onepixel2025@gmail.com>
2025-11-10 18:29:14 +0200

    Update chatbot widget

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -5,7 +5,13 @@ import DashboardProvider from './provider'
 function DashboardLayout({ children }) {
     return (
         <>
-            <Script src="https://elfsightcdn.com/platform.js" strategy="afterInteractive" />
+            <Script
+                src="https://app.tubetoolsai.com/Modules/Chatbot/Resources/assets/js/chatbot-widget.min.js"
+                strategy="afterInteractive"
+                data-iframe-src="https://app.tubetoolsai.com/chatbot/embed/chatbot_code=bfe044541ece4c8/welcome"
+                data-iframe-height="532"
+                data-iframe-width="400"
+            />
             <div suppressHydrationWarning={true}>
                 <DashboardProvider>
                     {children}
